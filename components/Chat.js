ebf1c3446e94518af4f577e1a62112a77119b862
twodozeneggs <simonjohnloik@gmail.com>
2022-06-21 21:38:27 -0500

    Initial Push

---
+++ components/Chat.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, Button, TextInput, Stylesheet } from 'react-native';
+
+
+
+export default class Chat extends React.Component {
+  render() {
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+    
+
+
+    return (
+      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+       <Button 
+       title='Go to Start'
+       onPress= {() => this.props.navigation.navigate("Start")}
+       />
+      </View>
+    );
+  }
+}
